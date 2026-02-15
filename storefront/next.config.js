5728d1fb63c4f087be9bb7673ef104fd49ff10a4
codemintah <86488838+Mintahandrews@users.noreply.github.com>
2026-02-15 09:24:32 +0000

    feat: add centralized image domain tracking with KNOWN_IMAGE_DOMAINS set
    - Create KNOWN_IMAGE_DOMAINS Set to track all configured image hostnames
    - Add addDynamicDomain helper to register hostnames from environment variables
    - Configure Next.js images.domains array from KNOWN_IMAGE_DOMAINS for better compatibility
    - Replace safeUrlToRemotePattern calls with addDynamicDomain for BASE_URL, MEDUSA_BACKEND_URL, and MINIO_ENDPOINT
    - Ensures all remote pattern hostnames are also registered in domains

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -50,6 +50,24 @@ const safeUrlToRemotePattern = (value) => {
   }
 }
 
+const KNOWN_IMAGE_DOMAINS = new Set([
+  "letscasegh.com",
+  "www.letscasegh.com",
+  "images.unsplash.com",
+  "i.pravatar.cc",
+  "medusa-public-images.s3.eu-west-1.amazonaws.com",
+  "medusa-server-testing.s3.amazonaws.com",
+  "medusa-server-testing.s3.us-east-1.amazonaws.com",
+])
+
+const addDynamicDomain = (value) => {
+  const pattern = safeUrlToRemotePattern(value)
+  if (pattern?.hostname) {
+    KNOWN_IMAGE_DOMAINS.add(pattern.hostname)
+  }
+  return pattern
+}
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -112,6 +130,7 @@ const nextConfig = {
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: Array.from(KNOWN_IMAGE_DOMAINS),
     remotePatterns: [
       {
         protocol: "http",
@@ -134,12 +153,12 @@ const nextConfig = {
         hostname: "www.letscasegh.com",
       },
       // Note: needed to serve images from /public folder when using absolute URLs.
-      ...(safeUrlToRemotePattern(process.env.NEXT_PUBLIC_BASE_URL)
-        ? [safeUrlToRemotePattern(process.env.NEXT_PUBLIC_BASE_URL)]
+      ...(addDynamicDomain(process.env.NEXT_PUBLIC_BASE_URL)
+        ? [addDynamicDomain(process.env.NEXT_PUBLIC_BASE_URL)]
         : []),
       // Note: only needed when using local-file for product media
-      ...(safeUrlToRemotePattern(process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL)
-        ? [safeUrlToRemotePattern(process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL)]
+      ...(addDynamicDomain(process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL)
+        ? [addDynamicDomain(process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL)]
         : []),
       // Note: can be removed after deleting demo products
       {
@@ -162,8 +181,8 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.pravatar.cc",
       },
-      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT
-        ? [safeUrlToRemotePattern(process.env.NEXT_PUBLIC_MINIO_ENDPOINT)].filter(Boolean)
+      ...(addDynamicDomain(process.env.NEXT_PUBLIC_MINIO_ENDPOINT)
+        ? [addDynamicDomain(process.env.NEXT_PUBLIC_MINIO_ENDPOINT)]
         : []),
     ].filter(Boolean),
   },
